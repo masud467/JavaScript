46674b5b554bf7fb360c911eb3c1d551daad3fc7
masud4657 <masudpfrnt@gmail.com>
2024-08-14 11:33:39 +0600

    added the switch

---
+++ JavaBasic/switch.js
@@ -0,0 +1,25 @@
+const month = "";
+switch (month) {
+  case "jan":
+    console.log("thanda");
+    return
+  case "feb":
+    console.log("thanda thanda");
+   return
+  case "mar":
+    console.log("hot");
+   return
+  case "apr":
+    console.log("hot hot");
+   return
+  case "may":
+    console.log("may day");
+    return
+  case "jun":
+    console.log("hot hot hot");
+   return
+
+  default:
+    console.log("phool phutuk ar na phutuk! aaj boshonto");
+    
+}
