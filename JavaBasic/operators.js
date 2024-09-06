3b1a34b9e74f813dfb4260ab3be8049b565621e8
masud4657 <masudpfrnt@gmail.com>
2024-09-06 11:07:34 +0600

    added the all arithmetic operators.

--- JavaBasic/operators.js
+++ JavaBasic/operators.js
@@ -1,3 +1,19 @@
 // addition
 var price = 10+10
 console.log(price)
+
+// subtraction
+var money=60-30
+console.log(money)
+
+// multiplication
+var fish= 2*350
+console.log(fish)
+
+// division
+var part = 60/4
+console.log(part)
+
+// remainder/modulus
+var extra= 65 % 4
+console.log(extra)
