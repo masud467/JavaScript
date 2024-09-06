ee9ad71475b988082b709d7bf18cefd2a8eba81b
masud4657 <masudpfrnt@gmail.com>
2024-09-06 11:20:19 +0600

    added the all operation shorthand methods.

---
+++ JavaBasic/operationShorthand.js
@@ -0,0 +1,29 @@
+// addition
+var money = 35;
+console.log(money)
+money += 25;
+console.log(money)
+
+// subtraction
+var money1 = 35;
+console.log(money1)
+money1 -= 25;
+console.log(money1)
+
+// multiplication
+var money2 = 35;
+console.log(money2)
+money2 *= 2;
+console.log(money2)
+
+// division
+var money3 = 35;
+console.log(money3)
+money3 /= 2;
+console.log(money3)
+
+// remainder/modulus
+var money4 = 35;
+console.log(money4)
+money4 %= 3;
+console.log(money4)
